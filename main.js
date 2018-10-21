d8a7635bb0715124d709cf40379e43307161b76b
Maxim Webb <maximwebb@gmail.com>
2018-10-21 01:19:38 +0100

    Add frequency analysis and copying to clipboard
    Frequency analysis will return results as a string, but also produces a sorted array of letter-percentage pairs.

--- main.js
+++ main.js
@@ -10,21 +10,58 @@ function setup() {
 
 function enterInput() {
 	inputText = document.getElementById('input').value;
-	let outputText = splitString(inputText, 4);
+	let outputText = freqAnalysis(inputText);
 	document.getElementById('output').value = outputText;
+}
+
+//Copies output to clipboard
+function copyOutput() {
+	outputText = document.getElementById('output');
+	outputText.select();
+	document.execCommand('copy');
 }
 
 //Removes spaces & punctuation from string
 function removePunc(str) {
-	return str.replace(/\W+/g, '');
+	return str.replace(/[^a-z]/g, '');
 }
 
 //Removes spaces & punctuation then splits string into groups of n.
 function splitString(str, n) {
 	str = removePunc(str);
-	str = str.split('');
-	for (i = n; i < str.length; i+= (n+1)) {
-		str.splice(i, 0, " ");
+	let arr = str.split('');
+	for (i = n; i < arr.length; i+= (n+1)) {
+		arr.splice(i, 0, " ");
 	}
-	return str.join('');
+	return arr.join('');
+}
+
+//Performs a frequency analysis on a string
+function freqAnalysis(str) {
+	let arr = removePunc(str).split('');
+	arr.sort();
+
+	let pos = arr[0];
+	let count = 0;
+	let output = "";
+	let outputArr = [];
+	for (i = 0; i < arr.length; i++) {
+		if (arr[i] !== pos) {
+			outputArr.push([]);
+			outputArr[outputArr.length - 1] = [arr[i-1], Math.round(count * 10000 / arr.length)/100];
+			output += arr[i-1] + ": " + (Math.round(count * 10000 / arr.length)/100) + "%, ";
+			count = 1;
+			pos = arr[i];
+		}
+		else if (i === arr.length - 1) {
+			outputArr[outputArr.length - 1] = [arr[i-1], Math.round(count * 10000 / arr.length)/100];
+			output += arr[i-1] + ": " + (Math.round(count * 10000 / arr.length)/100) + "%";
+		}
+		else {
+			count++;
+		}
+	}
+	outputArr.sort( (a, b) => b[1] - a[1]);
+	console.log(outputArr);
+	return output;
 }
