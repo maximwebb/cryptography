7179f8936affa0d195c61b2d12dd51dc7a549c0d
Maxim Webb <maximwebb@gmail.com>
2018-10-22 18:28:08 +0100

    Fix flexbox container for input/output/copy button elements
    -Copy button now contained within flex-container
    -Added test barchart function
    -Fix Caesar shift button not working

--- ux-commands.js
+++ ux-commands.js
@@ -34,7 +34,7 @@ function execMethod(command, params = []) {
 		outputText = splitString(inputText, params[0]);
 	}
 	else if (command === 'caesarShift') {
-		outputText = caesarShift(inputText, params[0]);
+		outputText = caesarShift(inputText, 'encrypt', params[0]);
 	}
 
 	document.getElementById('output').value = outputText;
@@ -63,13 +63,43 @@ function copyOutput() {
 	document.execCommand('copy');
 }
 
-//Displays a bar chart
+///Displays a bar chart
 function barChart(data, xAxis, yAxis) {
 	var ctx = document.getElementById('outputChart').getContext('2d');
 	var outputChart = new Chart(ctx, {
 		type: 'bar',
 		data: {
-
+			labels: ["A", "B", "C", "D", "E"],
+			datasets: [{
+				label: yAxis,
+				data: data,
+				backgroundColor: [
+					'rgba(255, 99, 132, 0.2)',
+					'rgba(54, 162, 235, 0.2)',
+					'rgba(255, 206, 86, 0.2)',
+					'rgba(75, 192, 192, 0.2)',
+					'rgba(153, 102, 255, 0.2)',
+					'rgba(255, 159, 64, 0.2)'
+				],
+				borderColor: [
+					'rgba(255,99,132,1)',
+					'rgba(54, 162, 235, 1)',
+					'rgba(255, 206, 86, 1)',
+					'rgba(75, 192, 192, 1)',
+					'rgba(153, 102, 255, 1)',
+					'rgba(255, 159, 64, 1)'
+				],
+				borderWidth: 1
+			}]
+		},
+		options: {
+			scales: {
+				yAxes: [{
+					ticks: {
+						beginAtZero: true
+					}
+				}]
+			}
 		}
 	});
 }
